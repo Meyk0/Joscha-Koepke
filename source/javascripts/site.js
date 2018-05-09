d00444ef4f8ff1be6df438ea1659e0a89499bc67
Joscha Koepke <joscha.koepke@gmail.com>
2018-05-09 22:49:22 +0100

    newwest version

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,7 @@
 // This is where it all goes :)
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
