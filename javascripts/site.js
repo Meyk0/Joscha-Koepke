7aa235cf61f5825c098654dcbfaae638160e148c
Joscha Koepke <joscha.koepke@gmail.com>
2018-05-16 16:43:43 +0100

    master

--- javascripts/site.js
+++
@@ -1,7 +0,0 @@
-// This is where it all goes :)
-$(function() {
-  $('a[href*=#]').on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-  });
-});
