5209f0a4731567fab3eff1ff99f2f52c3f41dce0
Joscha Koepke <joscha.koepke@gmail.com>
2018-05-17 10:03:54 +0100

    build

---
+++ javascripts/site.js
@@ -0,0 +1,29 @@
+// This is where it all goes :)
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
+
+;(function($, win) {
+  $.fn.inViewport = function(cb) {
+     return this.each(function(i,el){
+       function visPx(){
+         var H = $(this).height(),
+             r = el.getBoundingClientRect(), t=r.top, b=r.bottom;
+         return cb.call(el, Math.max(0, t>0? H-t : (b<H?b:H)));
+       } visPx();
+       $(win).on("resize scroll", visPx);
+     });
+  };
+}(jQuery, window));
+
+
+$(document).ready(function(){
+  $(".progress > div").inViewport(function(px){
+    console.log(px);
+    if(px) $(this).addClass("progress-bar") ;
+  });
+
+});
